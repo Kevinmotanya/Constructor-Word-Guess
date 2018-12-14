3590fac9fdadb6918fbe3d5236cb9d10fc04c9af
Kevinmotanya <42073382+Kevinmotanya@users.noreply.github.com>
2018-12-14 15:53:47 -0600

    Create word.js

---
+++ word.js
@@ -0,0 +1,19 @@
+/ var letters = require("/letter.js");
+
+
+var Word = function (letter) {
+    this.listOfWords = [];
+    this.currentWord = function(){
+        //math.random....to choose a random word from array
+    }
+
+    
+
+        
+    
+       
+        
+//     });
+// }
+
+// Game();
